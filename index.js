03c127cdc5641e3f2935d9c502a7673abd7c4f0c
ThomasT <thomastyack@aceik.com.au>
2019-04-20 22:56:41 +1000

    Any URL

--- index.js
+++ index.js
@@ -7,58 +7,69 @@ const express = require('express');
 
 let IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
-let getBrowser = () => puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io', ignoreHTTPSErrors: true, args: ['--disable-setuid-sandbox', '--no-sandbox'],defaultViewport: {width: 1300, height: 1500} });
+let getBrowser = () => puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io', ignoreHTTPSErrors: true, args: ['--disable-setuid-sandbox', '--no-sandbox']
+	//,defaultViewport: {width: 1800, height: 1200} 
+});
 
-// if(!IS_PRODUCTION)
-// {
-	// console.log("local environment launching");
-	// getBrowser = () => puppeteer.launch({
-	  // ignoreHTTPSErrors: true,
-	  // args: ['--disable-setuid-sandbox', '--no-sandbox'],
-	  // // not required to specify here, but saves Penthouse some work if you will
-	  // // re-use the same viewport for most penthouse calls.
-	  // defaultViewport: {
-		// width: 1300,
-		// height: 900
-	  // }
-	// });
-// }
+if(!IS_PRODUCTION)
+{
+	console.log("local environment launching");
+	getBrowser = () => puppeteer.launch({
+	  ignoreHTTPSErrors: true,
+	  args: ['--disable-setuid-sandbox', '--no-sandbox'],
+	  // not required to specify here, but saves Penthouse some work if you will
+	  // re-use the same viewport for most penthouse calls.
+	  //defaultViewport: {width: 1800, height: 1200} 
+	});
+}
 
 const app = express();
 
-function criticalReader(res) {
+function criticalReader(res, url, width, height) {
+	  
+	  let decodedUrl = decodeURIComponent(url);
+	  
+	  console.log("Looking up URL " + decodedUrl);
+	  
+	  res.set('Content-Type', 'application/json');
+	  res.type('application/json');
+	  
 	  try {
 		  var criticalresult = critical.generate({
-			src: 'https://www.example.com/',
+			src: decodedUrl,
 			minify: true,
 			inline: false,
 			extract: false,
 			timeout: 30000,
 			penthouse: {
-			  url: 'https://www.example.com/',
-			  cssstring: '',
+			  url: decodedUrl,
+			  cssString: '',
 			  puppeteer: {
-				getbrowser: getBrowser
+				getBrowser: getBrowser
 			  }
 			},
-			width: 1300,
-			height: 1500
+			width: width,
+			height: height
 		}).then(function (result) {
-			console.log("promise resolved");
-			console.log("criticalresult -> " + typeof result);
-			res.end(result+'\n');
-		}).catch(function () {
+			console.log("promise resolved");			
+			res.status(200);
+			res.send({ result: result });
+			res.end();
+		}).catch(function (err) {
 			 console.log("promise rejected");
+			 res.status(500);
+			 res.json({ error: err }).end();
 		}); 
 			
 		console.log("completed");
 	  } catch (err) {
-		console.log(" my  ----- error " + err);
+		res.status(500);
+		res.json({ error: err }).end();
 	  }
 }
 
-app.get('/image', async (req, res) => {
-	criticalReader(res);
+app.get('/critical/:url/:width/:height', async (req, res) => {
+	criticalReader(res, req.params.url, req.params.width, req.params.height);
     return;
 });
 
